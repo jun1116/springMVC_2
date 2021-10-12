132615c6ad3f0c5b68f5553153c54ad6b0566238
hj <jun1057@ajou.ac.kr>
2021-10-12 22:00:39 +0900

    변수사용 (Object,List,Map), 지역변수 선언

--- thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
+++ thymeleaf-basic/src/main/java/hello/thymeleaf/basic/BasicController.java
@@ -1,5 +1,7 @@
 package hello.thymeleaf.basic;
 
+
+import lombok.Data;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +9,26 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Controller
 @RequestMapping("/basic")
 public class BasicController {
+    @Data
+    static class User{
+        private String username;
+        private int age;
+
+        public User(String username, int age) {
+            this.username = username;
+            this.age = age;
+        }
+    }
+
     @GetMapping("/text-basic")
     public String textBasic(Model model) {
 //        model.addAttribute("data", "hello Spring");
@@ -28,5 +47,28 @@ public class BasicController {
     public String textUnescaped(Model model) {
         model.addAttribute("data", "hello <b>Spring</b>");
         return "basic/text-unescaped";
+    }
+    @GetMapping("/variable")
+    public String variable(Model model) {
+        User userA = new User("userA", 10);
+        User userB = new User("userB", 20);
+        List<User> list = new ArrayList<>();
+        list.add(userA);
+        list.add(userB);
+
+        Map<String, User> map = new HashMap<>();
+        map.put("userA", userA);
+        map.put("userB", userB);
+
+        model.addAttribute("user", userA);
+        model.addAttribute("users", list);
+        model.addAttribute("userMap", map);
+
+        return "basic/variable";
+    }
+
+    @GetMapping("/basic-objects")
+    public String basicObjects(HttpSession session) {
+
     }
 }
